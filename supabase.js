181979171ad803808c89f2a653ebfa4536fc0561
amalsus1995-cpu <am.alsus1995@gmail.com>
2025-12-12 18:44:59 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 const SUPABASE_URL = "https://hpcipbpgsmvwmkseqfbk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_cnwHtamgdBeaN5XrIa0RmA_UAaxCGnS";
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_ANON_KEY = "sb_publishable_...";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
